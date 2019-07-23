bf1e4bf5adcbf5cd589a4f00a860143a3d8c3ebf
Zoehw <52467391+Zoehw@users.noreply.github.com>
2019-07-22 21:20:18 -0400

    Add files via upload

---
+++ github/assets/data/myBooks.ts
@@ -0,0 +1,9 @@
+import { Books } from '../../app/bookInfo';
+
+export const MYBOOKS: Books[] =
+[
+    {bname: 'The Lord of the Rings', bauthor: 'J. R. R. Tolkien', bgenre: 'Fantasy Novel', byear: '1955', bpic: 'assets/images/LOR.jpg'},
+    {bname: 'The Hobbit', bauthor: 'J. R. R. Tolkien', bgenre: 'Fantasy Novel', byear: '1937', bpic: 'assets/images/Hobbit.jpg'},
+    {bname: 'Chamber of Secrets', bauthor: 'J. K. Rowling', bgenre: 'Detective Fiction', byear: '1998', bpic: 'assets/images/HPC.jpg'},
+    {bname: 'Half-Blood Prince', bauthor: 'J. K. Rowling', bgenre: 'Fantasy Fiction', byear: '2005', bpic: 'assets/images/HPH.jpg'}
+]
